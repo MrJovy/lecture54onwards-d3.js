3c33b19884579261719baf9a984ad18d4991d52c
MrJovy <47345645+MrJovy@users.noreply.github.com>
2020-07-14 16:14:06 -0400

    Pie chart set up

--- src/app.js
+++ src/app.js
@@ -7,3 +7,44 @@ import './style.css';
 var data = [25, 25, 25, 25];
 var chart_width = 600;
 var chart_height = 600;
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var pie = d3.pie();
+var outer_radius = chart_width / 2;
+var inner_radius = 0;
+var arc = d3.arc()
+    .innerRadius(inner_radius)
+    .outerRadius(outer_radius);
+
+var svg = d3.select("#chart")
+    .append('svg')
+    .attr('width', chart_width)
+    .attr('height', chart_height);
+
+// groups
+var arcs = svg.selectAll('g.arc')
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .attr('class', 'arc')
+    .attr(
+        'transform',
+        "translate(" + outer_radius + "," + chart_height / 2 + ")"
+    );
+
+//Arcs
+arcs.append('path')
+    .attr('fill', function (d, i) {
+        return color(i);
+    })
+    .attr('d', arc);
+
+//Labels
+arcs.append('text')
+    .attr('transform', function (d, i) {
+        return 'translate(' + arc.centroid(d) + ')';
+    })
+    .attr('text-anchor', 'text-middle')
+    .text(function (d) {
+        return d.value;
+    })
