626d8f31051cee92affd8906afed1ead4e8448d5
MrJovy <47345645+MrJovy@users.noreply.github.com>
2020-08-02 10:43:48 -0400

    Render centered and scaled US Map

--- src/app.js
+++ src/app.js
@@ -6,28 +6,24 @@ import './us.json'; //! need to import the json script
 var chart_width = 800;
 var chart_height = 600;
 
-//! Projection 
-var path = d3.geoPath()
-    .projection(d3.geoAlbers());
 //!Create SVG
 var svg =
     d3.select("#chart")
     .append("svg")
     .attr("width", chart_width)
-    .attr("height", chart_height)
-
-console.log("bye");
+    .attr("height", chart_height);
 
 //! Data
-// d3.json('geo_data.json', function (data) {
-//     svg.selectAll('path')
-//         .data(data)
-//         .enter()
-//         .append('path')
-//         .attr('d', path);
-// });
-
 d3.json('/src/us.json').then(function (data) {
+    //! Projection 
+    var projection = d3.geoAlbersUsa()
+        .scale(1)
+        .fitSize([chart_width, chart_height], data);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+
     svg.selectAll('path')
         .data(data.features)
         .enter()
